ed25f2281d0128a5aa696fa91e0ad3d0948f73d3
shivam <shivammaur59@gmail.com>
2023-10-02 10:13:03 +0530

    about

--- src/components/About.js
+++ src/components/About.js
@@ -28,7 +28,7 @@ class About extends React.Component {
       </p>
       </div>
       {/* <Profile name="Ashish"/> */}
-      <ProfileClass fname="Ashish " lname={"Saha"} age={23} />
+      <ProfileClass fname="Shivam " lname={"Maurya"} age={22} />
       </>
     )
   }
